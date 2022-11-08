0a5715ae6f869e59777921eca2d9cecea6ad641f
Tony Ehlert <aehlert@dmacc.edu>
2022-11-08 16:43:48 -0600

    Update animal.java
    Change lastFed variable to two separate variables to make the time/date conversion easier

--- src/main/java/adoption/beans/animal.java
+++ src/main/java/adoption/beans/animal.java
@@ -1,11 +1,17 @@
 package adoption.beans;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -27,10 +33,19 @@ public class Animal {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	private Integer age;
 	private String name;
+	private String description;
 	
-	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss a")
-	private LocalDateTime lastFed;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate lastFedDate;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+	private LocalTime lastFedTime;
+	
+	
+	@ManyToMany(cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER)
+	private List<AnimalAttribute> listOfAttributes;
 	
 	private boolean available;
 }
